07bec96d5c3556e67b1fdfb24a8ca2594af2128d
Kangnos <rkdtlsgur66@gmail.com>
2020-07-20 23:07:10 +0900

    2020 07 20

--- Maker/musiclist_Maker.py
+++ Maker/musiclist_Maker.py
@@ -1,1 +1,14 @@
-import path
+import re
+MainList = open("Musics/HiphopList", "w")
+PlatformList_a = open("Maker/Hiphop", "r")
+PlatformList = PlatformList_a.read();
+PlatformList = PlatformList.strip()
+PlatformList = re.split(":|\n|:\n", PlatformList)
+
+MusicsToAddList = []
+for i in range(1, len(PlatformList)):
+    data = "\n"+"{\n" + "    vid: " + '"' + PlatformList[i] + '"\n' + "    artist: " + '"' + PlatformList[i-2] + '"\n' + "    musictitle: " + '"' + PlatformList[i-1] + '"' + "\n},"
+
+print(MusicsToAddList)
+print(len(PlatformList))
+print(data)
