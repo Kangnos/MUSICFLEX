4dcb64bebeecfe0d7b9cbc7bfa5cb7cabba00787
Kangnos <rkdtlsgur66@gmail.com>
2020-07-23 21:16:30 +0900

    2020 07 23

--- Maker/musiclist_Maker.py
+++ Maker/musiclist_Maker.py
@@ -7,9 +7,13 @@ PlatformList = re.split(":|\n|:\n", PlatformList)
 
 Musicdata = ""
 
-for i in range(0, len(PlatformList)):
-    Musicdata += "\n"+"{\n" + "    vid: " + '"' + PlatformList[i+1] + '",\n' + "    artist: " + '"' + PlatformList[i] + '",\n' + "    musictitle: " + '"' + PlatformList[i+1] + '"' + "\n},"
+for i in range(0, (len(PlatformList)// 3)):
+    if i == 0:
+        Musicdata += "\n"+"{\n" + "    vid: " + '"' + PlatformList[i+2] + '",\n' + "    artist: " + '"' + PlatformList[i] + '",\n' + "    musictitle: " + '"' + PlatformList[i+1] + '"' + "\n},"
+    elif i != 0:
+        Musicdata += "\n"+"{\n" + "    vid: " + '"' + PlatformList[i+4] + '",\n' + "    artist: " + '"' + PlatformList[i+3] + '",\n' + "    musictitle: " + '"' + PlatformList[i+2] + '"' + "\n},"
 # MainList.write(Musicdata)
 print(Musicdata)
 print(PlatformList)
 print(len(PlatformList))
+print("\n"+"{\n" + "    vid: " + '"' + PlatformList[i+2] + '",\n' + "    artist: " + '"' + PlatformList[i] + '",\n' + "    musictitle: " + '"' + PlatformList[i+1] + '"' + "\n},")
