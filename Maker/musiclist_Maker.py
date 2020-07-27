cb466a67e76d74409348801b5991f5f9f19b819f
Kangnos <rkdtlsgur66@gmail.com>
2020-07-27 23:49:54 +0900

    2020 07 27

--- Maker/musiclist_Maker.py
+++ Maker/musiclist_Maker.py
@@ -11,10 +11,13 @@ artistnum = 0
 musicnum = 2
 
 for i in range(0, (len(PlatformList)// 3)):
-    Musicdata += "\n"+"{\n" + "    artist: " + '"' + PlatformList[artistnum] + '",\n' + "    musictitle: " + '"' + PlatformList[vidnum] + '",\n' + "    vid: " + '"' + PlatformList[musicnum] + '"' + "\n},"
-    vidnum += 3
-    artistnum += 3
-    musicnum += 3
+    if i == 0:
+        Musicdata += "{\n" + "    artist: " + '"' + PlatformList[artistnum] + '",\n' + "    musictitle: " + '"' + PlatformList[vidnum] + '",\n' + "    vid: " + '"' + PlatformList[musicnum] + '"' + "\n},"
+    else:
+        vidnum += 3
+        artistnum += 3
+        musicnum += 3
+        Musicdata += "\n"+"{\n" + "    artist: " + '"' + PlatformList[artistnum] + '",\n' + "    musictitle: " + '"' + PlatformList[vidnum] + '",\n' + "    vid: " + '"' + PlatformList[musicnum] + '"' + "\n},"
 MainList.write(Musicdata)
 print(Musicdata)
 print(PlatformList)
