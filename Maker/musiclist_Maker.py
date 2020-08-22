f7781b060d327b7e2e508adaf6893fb3ac507cee
Kangnos <rkdtlsgur66@gmail.com>
2020-08-22 17:30:37 +0900

    2020 08 22

--- Maker/musiclist_Maker.py
+++ Maker/musiclist_Maker.py
@@ -14,25 +14,31 @@ Pop_PlatformList = re.split(":|\n|:\n", Pop_PlatformList)
 Musicdata = ["", ""]
 
 artistnum = 0
-vidnum = 1
-musicnum = 2
+musicnum = 1
+vidnum = 2
 
 janre = [Hiphop_PlatformList, Pop_PlatformList]
+
+overlap_songs = ""
+
 
 for j in range(0,(len(janre))):
     for i in range(len(janre[j])//3): # This part is the problem that I have to fix today or tomorrow. I can nailed it!
         if i == 0: 
-            Musicdata[j] += ("{\n" + "    artist: " + '"' + janre[j][artistnum] + '",\n' + "    musictitle: " + '"' + janre[j][vidnum] + '",\n' + "    vid: " + '"' + janre[j][musicnum] + '"' + "\n},")
+            Musicdata[j] += ("{\n" + "    artist: " + '"' + janre[j][artistnum] + '",\n' + "    musictitle: " + '"' + janre[j][musicnum] + '",\n' + "    vid: " + '"' + janre[j][vidnum] + '"' + "\n},")
         else:
             vidnum += 3
             artistnum += 3
             musicnum += 3
-            Musicdata[j] += ("\n"+"{\n" + "    artist: " + '"' + janre[j][artistnum] + '",\n' + "    musictitle: " + '"' + janre[j][vidnum] + '",\n' + "    vid: " + '"' + janre[j][musicnum] + '"' + "\n},")
+            Musicdata[j] += ("\n"+"{\n" + "    artist: " + '"' + janre[j][artistnum] + '",\n' + "    musictitle: " + '"' + janre[j][musicnum] + '",\n' + "    vid: " + '"' + janre[j][vidnum] + '"' + "\n},")
+            print(janre[j][artistnum]+"-"+janre[j][musicnum], Musicdata[j].count(janre[j][musicnum]))
     artistnum = 0
-    vidnum = 1
-    musicnum = 2
+    musicnum = 1
+    vidnum = 2
     if j == 0:
         HiphopList.write(Musicdata[0])
     if j == 1:
         PopList.write(Musicdata[1])
-print(Musicdata)
+# print(Musicdata)
+# overlap_songs += Musicdata.count(janre[0].musicnum)
+print("중복되는 노래: ", overlap_songs)
