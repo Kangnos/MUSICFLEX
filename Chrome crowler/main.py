b11ee0a031f1af02a6dcaab8803fb7d98146bd57
Kangnos <rkdtlsgur66@gmail.com>
2020-08-27 23:27:07 +0900

    2020 08 27

--- Chrome crowler/main.py
+++ Chrome crowler/main.py
@@ -4,26 +4,28 @@ import time
 import requests
 from bs4 import BeautifulSoup
 import time
+import urllib.request
+from urllib import request,response
+import re
 
-browser = webdriver.Chrome('.\chrome crowling test\chromedriver.exe')
+browser = webdriver.Chrome('Chrome crowler\chromedriver.exe')
 
-delay=3
+delay=2
 browser.implicitly_wait(delay)
 
-start_url  = 'https://www.youtube.com'
+start_url  = 'https://music.youtube.com'
 browser.get(start_url)  
 browser.maximize_window()
 
-browser.find_elements_by_xpath('//*[@id="search-input"]')[0].click()
-browser.find_elements_by_xpath('//*[@id="search-form"]/div/div/div/div[2]/input')[0].send_keys('Rap god audio')#검색창 영역에 원하는 youtuber입력
-browser.find_elements_by_xpath('//*[@id="search-form"]/div/div/div/div[2]/input')[0].send_keys(Keys.RETURN)#엔터
 
-browser.find_elements_by_xpath('//*[@class="channel-link yt-simple-endpoint style-scope ytd-channel-renderer"]')[0].click()
+browser.find_elements_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]')[0].click()
+browser.find_elements_by_id('input')[1].send_keys('Eminem Rap god') #검색창 영역에 원하는 youtuber입력
+browser.find_elements_by_id('input')[1].send_keys(Keys.RETURN)#엔터
 
-browser.find_element_by_xpath('//*[@class="scrollable style-scope paper-tabs"]/paper-tab[2]').click()
-
-body = browser.find_element_by_tag_name('body')#스크롤하기 위해 소스 추출
+browser.find_element_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/div[3]/ytmusic-search-page/ytmusic-section-list-renderer/div[2]/ytmusic-shelf-renderer[2]/div[2]/ytmusic-responsive-list-item-renderer[1]/div[1]/ytmusic-item-thumbnail-overlay-renderer/div/ytmusic-play-button-renderer/div').click()
 
 
-html0 = browser.page_source
-html = BeautifulSoup(html0,'html.parser')
+current_url = browser.current_url
+
+videocode = re.split('=|&', current_url)
+videocode = videocode[1]
--- Maker/Music to add in list/mover.py
+++ Maker/Music to add in list/mover.py
@@ -21,10 +21,41 @@ janre = [Hiphop_PlatformList, Pop_PlatformList]
 
 overlap_songs = ""
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import requests
+from bs4 import BeautifulSoup
+import time
+import urllib.request
+from urllib import request,response
+import re
+
+browser = webdriver.Chrome('Chrome crowler\chromedriver.exe')
+
+delay=2
+browser.implicitly_wait(delay)
+
+start_url  = 'https://music.youtube.com'
+browser.get(start_url)  
+browser.maximize_window()
+
+browser.find_elements_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]')[0].click()
+browser.find_elements_by_id('input')[1].send_keys('Eminem Rap god') #검색창 영역에 원하는 youtuber입력
+browser.find_elements_by_id('input')[1].send_keys(Keys.RETURN)#엔터
+
+browser.find_element_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/div[3]/ytmusic-search-page/ytmusic-section-list-renderer/div[2]/ytmusic-shelf-renderer[2]/div[2]/ytmusic-responsive-list-item-renderer[1]/div[1]/ytmusic-item-thumbnail-overlay-renderer/div/ytmusic-play-button-renderer/div').click()
+
+
+current_url = browser.current_url
+
+videocode = re.split('=|&', current_url)
+videocode = videocode[1]
+
 
 for j in range(0,(len(janre))):
     for i in range(len(janre[j])//3): # This part is the problem that I have to fix today or tomorrow. I can nailed it!
-        Musicdata[j] += (janre[j][artistnum]+ " - " + janre[j][musicnum] + "\n")
+        Musicdata[j] += (janre[j][artistnum]+ ":" + janre[j][musicnum] + "\n")
         musicnum += 3
         artistnum += 3
     artistnum = 0
