790bc79c0f0438b82269d34583c91e88561784a2
Kangnos <rkdtlsgur66@gmail.com>
2020-08-28 22:54:18 +0900

    2020 08 28

--- Maker/Main/musiclist_Maker.py
+++ Maker/Main/musiclist_Maker.py
@@ -1,48 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-import requests
-from bs4 import BeautifulSoup
-import time
-import urllib.request
-from urllib import request,response
 import re
-
-browser = webdriver.Chrome('Chrome crowler\chromedriver.exe')
-
-delay=2
-browser.implicitly_wait(delay)
-
-start_url  = 'https://music.youtube.com'
-browser.get(start_url)  
-browser.maximize_window()
-
-
-browser.find_elements_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]')[0].click()
-browser.find_elements_by_id('input')[1].send_keys('Eminem Rap god') #검색창 영역에 원하는 youtuber입력
-browser.find_elements_by_id('input')[1].send_keys(Keys.RETURN)#엔터
-
-browser.find_element_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/div[3]/ytmusic-search-page/ytmusic-section-list-renderer/div[2]/ytmusic-shelf-renderer[2]/div[2]/ytmusic-responsive-list-item-renderer[1]/div[1]/ytmusic-item-thumbnail-overlay-renderer/div/ytmusic-play-button-renderer/div').click()
-
-
-current_url = browser.current_url
-
-videocode = re.split('=|&', current_url)
-videocode = videocode[1]
-
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-import re
-
 HiphopList = open("Musics/HiphopList", "w", encoding='UTF-8')
-Hiphop_PlatformList_a = open("Maker/Main/Hiphop", "r", encoding='UTF-8')
+Hiphop_PlatformList_a = open("Maker\Main\Hiphop", "r", encoding='UTF-8')
 Hiphop_PlatformList = Hiphop_PlatformList_a.read();
 Hiphop_PlatformList = Hiphop_PlatformList.strip()
 Hiphop_PlatformList = re.split(":|\n|:\n", Hiphop_PlatformList)
 
 PopList = open("Musics/PopList", "w", encoding='UTF-8')
-Pop_PlatformList_a = open("Maker/Main/Pop", "r", encoding='UTF-8')
+Pop_PlatformList_a = open("Maker\Main\Pop", "r", encoding='UTF-8')
 Pop_PlatformList = Pop_PlatformList_a.read();
 Pop_PlatformList = Pop_PlatformList.strip()
 Pop_PlatformList = re.split(":|\n|:\n", Pop_PlatformList)
@@ -59,18 +23,7 @@ overlap_songs = ""
 
 
 for j in range(0,(len(janre))):
-    for i in range(len(janre[j])//3): 
-                browser.find_elements_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]')[0].click()
-        browser.find_elements_by_id('input')[1].send_keys(janre[j][artistnum] + janre[j][musicnum] + " audio") #검색창 영역에 원하는 youtuber입력
-        browser.find_elements_by_id('input')[1].send_keys(Keys.RETURN)#엔터
-
-        browser.find_element_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/div[3]/ytmusic-search-page/ytmusic-section-list-renderer/div[2]/ytmusic-shelf-renderer[2]/div[2]/ytmusic-responsive-list-item-renderer[1]/div[1]/ytmusic-item-thumbnail-overlay-renderer/div/ytmusic-play-button-renderer/div').click()
-
-
-        current_url = browser.current_url
-
-        videocode = re.split('=|&', current_url)
-        videocode = videocode[1]
+    for i in range(len(janre[j])//3): # This part is the problem that I have to fix today or tomorrow. I can nailed it!
         if i == 0: 
             Musicdata[j] += ("{\n" + "    artist: " + '"' + janre[j][artistnum] + '",\n' + "    musictitle: " + '"' + janre[j][musicnum] + '",\n' + "    vid: " + '"' + janre[j][vidnum] + '"' + "\n},")
         else:
